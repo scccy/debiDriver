158fa382dfd9771ad8ef0b373841276d9e2cb5d3
scccy <shici2880@gmail.com>
2025-07-14 21:39:31 +0800

    123

---
+++ src/main/java/com/scccy/debidriver/DebiDriverApplication.java
@@ -0,0 +1,13 @@
+package com.scccy.debidriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebiDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebiDriverApplication.class, args);
+    }
+
+}
